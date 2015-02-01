5da979b7e16de4b8bdf1c9155793bd62a9536fea
Ethan Sarp <killershmeeps@yahoo.com>
2015-02-01 01:41:22 -0600

    None

--- src/Shmeeps/PHPAcl/AclActor.php
+++ src/Shmeeps/PHPAcl/AclActor.php
@@ -9,17 +9,35 @@ namespace Shmeeps\PHPAcl;
  */
 abstract class AclActor {
 
+    // Groups the user belongs to
+    protected $groupHierarchy = array();
+
     /**
      * @return integer
      */
     abstract public function getAclActorId();
 
     /**
-     * @param IAclObject $object Object to check access against
-     * @param AclAction  $action Action to check access against
+     * @param IAclObject    $object    Object to check access against
+     * @param AclAction     $actionId  Action to check access against
      * @return boolean
      */
-    public function hasAccess(IAclObject $object, AclAction $action) {
+    public function hasAccess(IAclObject $object, AclAction $actionId) {
+
+        // Get actor and object data
+        $actorId = $this->getAclActorId();
+        $objectId = $object->getAclObjectId();
+
+        // TODO: Get Actor permissions, check them, those override everything
+
+        // TODO: If no actor stuff, get groups and their permissions
+    }
+
+    /**
+     * Get groups the user belongs too in a hierarchy so they can be
+     * transversed for inherited permissions
+     */
+    public function getGroupHierarchy() {
 
     }
 }
